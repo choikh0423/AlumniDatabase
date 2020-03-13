5aa87f293f2c415ad2ebbd0dc2c642e258329a6c
Kyu Hwan Choi <kyuhwanchoi0423@gmail.com>
2020-03-12 20:28:57 -0400

    Alumni Model Update

--- alumnidatabase/searches/models.py
+++ alumnidatabase/searches/models.py
@@ -3,9 +3,33 @@ from django.db import models
 # Create your models here.
 
 
-class Alumni(models.Model):
-    COLLEGE_CHOICES = [('CALS', 'Collge of Agriculture and Life Sciences'),
-                       ('COE', 'College of Engineering')]
+class College(models.Model):
+    """Model representing a College within university."""
     name = models.CharField(max_length=100)
-    college = models.CharField(choices=COLLEGE_CHOICES, max_length=4)
+
+
+class Major(models.Model):
+    """Model representing a Major."""
+    name = models.CharField(max_length=100)
+
+
+class Industry(models.Model):
+    """Model representing a Industry."""
+    name = models.CharField(max_length=100)
+
+
+class Employer(models.Model):
+    """Model representing a Employer."""
+    name = models.CharField(max_length=100)
+
+
+class Location(models.Model):
+    """Model representing a Location."""
+    name = models.CharField(max_length=100)
+
+
+class Alumni(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField()
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
     graduation_date = models.IntegerField()
