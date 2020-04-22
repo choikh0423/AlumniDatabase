38052dd7fa9ca3c06e1036d2aa32121b11f204ce
Kyu Hwan Choi <57926472+choikh0423@users.noreply.github.com>
2020-04-22 10:54:16 +0900

    Revert "Userlogin/Passwordreset finalized"

--- alumnidatabase/searches/urls.py
+++ alumnidatabase/searches/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, re_path
 from django.urls import include
-
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('accounts/password_reset/',
-         views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
 ]
+
+# path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
