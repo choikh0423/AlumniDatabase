34dbd6488df38375edad299d09fc64057f87107e
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-01 11:57:53 +0900

    Enhanced frontend for login & implementing search page
    The frontend part for each page is in progress. It is not complete yet.

--- alumnidatabase/searches/urls.py
+++ alumnidatabase/searches/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -14,3 +15,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
